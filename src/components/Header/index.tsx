3c9224e5fe3cfa0abfb11f8ea1d4d22256c0c4a5
Parimal Kashireddy <pkashi1@lsu.edu>
2025-06-20 16:45:01 +0530

    added more images for reference and making background better

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -64,7 +64,7 @@ const Header = () => {
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.svg"
+                  src="/images/logo/logo.jpg"
                   alt="logo"
                   width={140}
                   height={30}
